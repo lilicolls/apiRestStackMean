db34c0f1390be45d8289367c677ce2aaf52423df
lilicolls <lilianacolls@gmail.com>
2018-03-19 20:52:41 +0000

    agregando metodo para insertar y obtener imagen de artista en mongo

--- routes/artist.js
+++ routes/artist.js
@@ -5,12 +5,15 @@ const ArtistController = require('../controllers/artist')
 
 const api = express.Router();
 const md_auth = require('../middlewares/aunthenticated')
-
+const multipart = require('connect-multiparty')
+const md_upload = multipart({uploadDir: './uploads/artists'})
 
 api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist)
 api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist)
 api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists) //? para decir que el parametro es opcional 
 api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist)
 api.delete('/artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist)
+api.post('/upload-image-artist/:id', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage)
+api.get('/get-image-artist/:imageFile', ArtistController.getImageFile)
 
 module.exports = api
