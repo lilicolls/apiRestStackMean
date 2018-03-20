81ab3f259a54d6853cfb759c46cc935815572ccb
lilicolls <lilianacolls@gmail.com>
2018-03-20 23:23:27 +0000

    agregando metodos y rutas para el controlador songs

--- controllers/song.js
+++ controllers/song.js
@@ -8,9 +8,160 @@ const Album = require('../models/album')
 const Song = require ('../models/song')
 
 function getSong(req,res){
-    res.status(200).send({message: 'controlador cancion'})
+    //obtener una cancion
+    const songId = req.params.id;
+
+    Song.findById(songId).populate({path: 'album'}).exec((err, song)=>{
+    
+        if (err){
+            res.status(500).send({message: 'error en la peticion'})
+        }else{
+            if(!song){
+                res.status(404).send({message: 'la cancion no existe'})
+            }else{
+                res.status(200).send({song})
+            }
+        }
+    })
+}
+
+function getSongs(req,res){
+    //metodo para listar las canciones
+    const albumId = req.params.id;
+
+    if(!albumId){
+        var find = Song.find({}).sort('number')
+    }else{
+        var find = Song.find({album: albumId}).sort('number')
+    }
+
+    find.populate({
+        path: 'album',
+        populate: {
+            path: 'artist',
+            models: 'Artist'
+        }
+    }).exec((err, songs)=>{
+        if (err){
+            res.status(500).send({message: 'error en la peticion'})
+        }else{
+            if(!songs){
+                res.status(404).send({message: 'no hay canciones almacenadas!'})
+            }else{
+                res.status(200).send({songs: songs})
+            }
+        }
+    })
+}
+
+function saveSong(req,res){
+    const song = new Song()
+    const params = req.body;
+
+    song.number = params.number;
+    song.name = params.name;
+    song.duration = params.duration;
+    song.file = null;
+    song.album = params.album
+
+
+    song.save((err,songStored)=>{
+        if (err){
+            res.status(500).send({message: 'error en la peticion'})
+        }else{
+            if(!songStored){
+                res.status(404).send({message: 'no se ha guardado la cancion'})
+            }else{
+                res.status(200).send({song: songStored})
+            }
+        }
+    })
+}
+
+function updateSong(req,res){
+    const songId = req.params.id;
+    const update = req.body;
+
+    Song.findByIdAndUpdate(songId, update, (err, songUpdated)=>{
+        if (err){
+            res.status(500).send({message: 'error en la peticion'})
+        }else{
+            if(!songUpdated){
+                res.status(404).send({message: 'no se ha actualizado la cancion'})
+            }else{
+                res.status(200).send({song: songUpdated})
+            }
+        }
+    })
+}
+
+function deleteSong(req,res){
+    const songId = req.params.id;
+
+    Song.findByIdAndRemove(songId, (err, songRemoved)=>{
+        if (err){
+            res.status(500).send({message: 'ocurrio un error en la peticion'})
+        }else{
+            if(!songRemoved){
+                res.status(404).send({message:'no se ha borrado la cancion'})
+            }else{
+                res.status(200).send({songRemoved})
+            }
+        }
+    })
+}
+
+function uploadFile (req,res){
+    var songId = req.params.id;
+	var file_name = 'No subido...';
+
+	if(req.files){
+		var file_path = req.files.file.path;
+		// console.log(file_path);
+		var file_split = file_path.split('\/');
+		var file_name = file_split[2];
+		// console.log(file_split);
+		var ext_split = (file_name.split('\.'));
+		var file_ext = ext_split[1];
+		// console.log(file_ext);
+
+		if((file_ext == 'mp3') || (file_ext == 'ogg')){
+			Song.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdated) => {
+				if(!songUpdated){
+					res.status(404).send({message: 'no se ha podido actualizar la cancion'});
+				}else{
+					res.status(200).send({message: songUpdated});
+				}
+			});
+		}else{
+			res.status(200).send({message: 'Extension del archivo no valida'});
+		}
+	}else{
+        res.status(200).send({message: 'No has subido ninguna cancion :('})
+    }
+}
+
+function getSongFile(req,res){
+    const songFile = req.params.songFile;
+    const path_file = './uploads/songs/' + songFile
+
+   
+    fs.exists(path_file,  (exists)=>{
+        if(exists){
+            res.status(200).sendFile(path.resolve(path_file))
+        }else{
+            res.status(404).send({message: 'la imagen no existe'})
+        }
+    })
+
 }
 
 module.exports = {
-    getSong
+    getSong,
+    saveSong,
+    getSongs,
+    updateSong,
+    deleteSong,
+    uploadFile,
+    getSongFile
 }
--- routes/song.js
+++ routes/song.js
@@ -7,6 +7,12 @@ const md_auth = require('../middlewares/aunthenticated')
 const multipart = require('connect-multiparty')
 const md_upload = multipart({uploadDir: './uploads/songs'})
 
-api.get('/song/:id?', md_auth.ensureAuth, SongController.getSong)
+api.get('/song/:id', md_auth.ensureAuth, SongController.getSong)
+api.post ('/song', md_auth.ensureAuth, SongController.saveSong)
+api.get('/songs/:album?', md_auth.ensureAuth, SongController.getSongs)
+api.put('/song/:id', md_auth.ensureAuth, SongController.updateSong)
+api.delete('/song/:id',md_auth.ensureAuth, SongController.deleteSong)
+api.post('/upload-file-song/:id', [md_auth.ensureAuth, md_upload], SongController.uploadFile)
+api.get('/get-song-file/:songFile', SongController.getSongFile)
 
 module.exports = api
