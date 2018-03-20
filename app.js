d1a298b5a7c9410f4c72bd984f98a163da6e4fd6
lilicolls <lilianacolls@gmail.com>
2018-03-20 23:37:31 +0000

    configuracion de cabeceras api-rest

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended:false}));
 app.use(bodyParser.json()); //convierte a obj json los datos de las peticiones recibidas por http
 
 // configurar cabeceras http
+app.use((req,res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X.Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next()
+})
+
 
 //ruta base
 app.use('/api', user_router)        
